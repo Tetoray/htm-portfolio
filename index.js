50fdea1c42392162d57d6a27f4041e6ad57f70c8
tarik rayan <120338572+Tetoray@users.noreply.github.com>
2023-08-29 13:35:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 
+
 var containers = document.querySelectorAll('.project-section');
 
 for (let i = 0; i < containers.length; i++) {
@@ -18,6 +19,27 @@ for (let i = 0; i < containers.length; i++) {
     container.addEventListener('mouseenter', addHover);
     container.addEventListener('mouseleave', removeHover);
 }
+
+var containers = document.querySelectorAll('.contact_card');
+
+for (let i = 0; i < containers.length; i++) {
+    let container = containers[i];
+
+    // Create a function that captures the current "container" value
+    function addHover() {
+        container.classList.add('hovered');
+    }
+
+    function removeHover() {
+        container.classList.remove('hovered');
+    }
+
+    // Add event listeners using the created functions
+    container.addEventListener('mouseenter', addHover);
+    container.addEventListener('mouseleave', removeHover);
+}
+
+
 
 
 
